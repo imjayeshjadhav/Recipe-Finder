d28df68fdc6fe47b9dbc45c7646a59468482fed5
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-07-12 22:32:01 +0530

    Delete and get routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import { ENV } from "./config/env.js"
 import { db } from "./config/db.js"
 import { favouritesTable } from "./db/schema.js"
+import { and, eq } from "drizzle-orm"
 
 const app = express()
 const PORT = ENV.PORT
@@ -33,6 +34,35 @@ app.post("/api/favourites", async (req,res) =>{
 
     } catch (error) {
         console.log("Error adding favourite", error)
+        res.status(500).json({error:"Something went wrong"})
+    }
+})
+
+app.get("/api/favourites/:userId", async (req, res) => {
+    try {
+
+        const {userId}  = req.params;
+        const userFavourites = await db.select().from(favouritesTable).where(eq(favouritesTable.userId, userId))
+        res.status(200).json(userFavourites);
+
+    } catch (error) {
+        console.log("Error fetching favourite", error)
+        res.status(500).json({error:"Something went wrong"})
+    }
+})
+
+app.delete("/api/favourites/:userId/:recipeId", async (req,res) => {
+    try {
+        const {userId, recipeId} = req.params
+
+        await db.delete(favouritesTable).where(
+            and(eq(favouritesTable.userId, userId), eq(favouritesTable.recipeId, parseInt(recipeId)) )
+        )
+
+        res.status(200).json({messsage :"Favourite removed successfully"})
+
+    } catch (error) {
+        console.log("Error removing favourite", error)
         res.status(500).json({error:"Something went wrong"})
     }
 })
