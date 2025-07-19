cced63f29251cd7992e81ffc75c37b78ac0a2fcd
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-07-19 13:47:49 +0530

    Building Home Screen featured section and MealAPI service

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,10 +1,173 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { useRouter } from "expo-router";
+import { useEffect, useState } from 'react';
+import { MealAPI } from '../../services/mealAPI';
+import  { homeStyles} from  "../../assets/styles/home.styles"
+import { Image } from 'expo-image';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../../constants/colors';
 
 const HomeScreen = () => {
+
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [featuredRecipe, setFeaturedRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadData = async () => {
+    try {
+      setLoading(true);
+
+      const [apiCategories, randomMeals, featuredMeal] = await Promise.all([
+        MealAPI.getCategories(),
+        MealAPI.getRandomMeals(12),
+        MealAPI.getRandomMeal(),
+      ]);
+
+      const transformedCategories = apiCategories.map((cat, index) => ({
+        id: index + 1,
+        name: cat.strCategory,
+        image: cat.strCategoryThumb,
+        description: cat.strCategoryDescription,
+      }));
+
+      setCategories(transformedCategories);
+
+      if (!selectedCategory) setSelectedCategory(transformedCategories[0].name);
+
+      const transformedMeals = randomMeals
+        .map((meal) => MealAPI.transformMealData(meal))
+        .filter((meal) => meal !== null);
+
+      setRecipes(transformedMeals);
+
+      const transformedFeatured = MealAPI.transformMealData(featuredMeal);
+      setFeaturedRecipe(transformedFeatured);
+    } catch (error) {
+      console.log("Error loading the data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadCategoryData = async (category) => {
+    try {
+      const meals = await MealAPI.filterByCategory(category);
+      const transformedMeals = meals
+        .map((meal) => MealAPI.transformMealData(meal))
+        .filter((meal) => meal !== null);
+      setRecipes(transformedMeals);
+    } catch (error) {
+      console.error("Error loading category data:", error);
+      setRecipes([]);
+    }
+  };
+
+  const handleCategorySelect = async (category) => {
+    setSelectedCategory(category);
+    await loadCategoryData(category);
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // await sleep(2000);
+    await loadData();
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={homeStyles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator ={false}
+        contentContainerStyle = {homeStyles.scrollContent}
+      >
+        <View style={homeStyles.welcomeSection}>
+          <Image
+            source={require("../../assets/images/food1.webp")}
+            style={{
+              width :100,
+              height:100
+            }}
+          />
+          <Image
+            source={require("../../assets/images/food2.webp")}
+            style={{
+              width :100,
+              height:100
+            }}
+          />
+          <Image
+            source={require("../../assets/images/food3.webp")}
+            style={{
+              width :100,
+              height:100
+            }}
+          />
+        </View>
+
+        {/* Featured Section */}
+        {
+          featuredRecipe && (
+            <View style={homeStyles.featuredSection}>
+              <TouchableOpacity 
+                style={homeStyles.featuredCard}
+                activeOpacity={0.9}
+                onPress={() => router.push(`recipe/${featuredRecipe.id}`)}
+              >
+                <View style={homeStyles.featuredImageContainer}>
+                  <Image
+                    source={{ uri : featuredRecipe.image}}
+                    style={homeStyles.featuredImage}
+                    contentFit='cover'
+                    transition={500}
+                  />
+                  <View style={homeStyles.featuredOverlay}>
+                    <View style={homeStyles.featuredBadge}>
+                      <Text style={homeStyles.featuredBadgeText}>Featured</Text>
+                    </View>
+                    
+                    <View style={homeStyles.featuredContent}>
+
+                      <Text style={homeStyles.featuredContent} numberOfLines={2}>
+                        {featuredRecipe.title}
+                      </Text>
+
+                      <View style={homeStyles.featuredMeta}>
+
+                        <View style={homeStyles.metaItem}>
+                          <Ionicons name="time-outline" size={16} color={COLORS.white} />
+                          <Text style={homeStyles.metaText}>{featuredRecipe.cookTime}</Text>
+                        </View>
+
+                        <View style={homeStyles.metaItem}>
+                          <Ionicons name="people-outline" size={16} color={COLORS.white} />
+                          <Text style={homeStyles.metaText}>{featuredRecipe.servings}</Text>
+                        </View>
+
+                        {featuredRecipe.area && (
+                          <View style={homeStyles.metaItem}>
+                            <Ionicons name="location-outline" size={16} color={COLORS.white} />
+                            <Text style={homeStyles.metaText}>{featuredRecipe.area}</Text>
+                          </View>
+                        )}
+
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )
+        }
+
+      </ScrollView>
     </View>
   )
 }
